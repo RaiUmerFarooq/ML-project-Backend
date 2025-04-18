4974ad242012162cacbfd55387fb7272bb19db00
Rai Umer <raifarooq7860786@gmail.com>
2025-04-19 04:54:39 +0500

    Adding the risk assessment of the student and from the Teacher

--- student_management/risk_analysis/urls.py
+++ student_management/risk_analysis/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'risk_analysis'
+
 urlpatterns = [
-    path('student/<int:student_id>/risk/', views.StudentRiskAnalysis.as_view(), name='student-risk'),
+    path('teacher/risk-analysis/<str:username>/', views.TeacherStudentRiskAnalysis.as_view(), name='teacher-risk-analysis'),
+    path('student/risk-analysis/', views.StudentRiskAnalysis.as_view(), name='student-risk-analysis'),
 ]
